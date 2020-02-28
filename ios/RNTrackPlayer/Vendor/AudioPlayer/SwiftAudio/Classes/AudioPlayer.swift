cba364bf8a619cf13a1495af3e923eac46744815
matlortie <42750673+matlortie@users.noreply.github.com>
2020-02-28 09:50:59 -0500

    Fix remote commands

--- ios/RNTrackPlayer/Vendor/AudioPlayer/SwiftAudio/Classes/AudioPlayer.swift
+++ ios/RNTrackPlayer/Vendor/AudioPlayer/SwiftAudio/Classes/AudioPlayer.swift
@@ -222,7 +222,7 @@ public class AudioPlayer: AVPlayerWrapperDelegate {
             self.enableRemoteCommands(item.getCommands())
         }
         else {
-            self.enableRemoteCommands(RemoteCommand.all())
+            self.enableRemoteCommands([.play, .pause, .stop, .togglePlayPause])
         }
     }
     
